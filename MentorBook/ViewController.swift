3631a78f1ac9c0964ce2b9cb63e68a3e6499afab
Kony <fzf55747@kwansei.ac.jp>
2021-02-14 17:42:04 +0900

    if文で落ちないようにするところまで完璧っ！！

--- MentorBook/ViewController.swift
+++ MentorBook/ViewController.swift
@@ -8,12 +8,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var mentorArray: [Mentor] = []
+    
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var courseLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    
+    var index: Int = 0
+    
+    
+    func setUI(){
+        imageView.image = mentorArray[index].getImage()
+        nameLabel.text = mentorArray[index].name
+        courseLabel.text = mentorArray[index].course
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mentorArray.append(Mentor(name: "Kony", imageName: "Kony.png", course: "Minecraft/Unity"))
+        mentorArray.append(Mentor(name: "See", imageName: "See.png", course: "Movie/Unity"))
+        mentorArray.append(Mentor(name: "TT", imageName: "TT.png", course: "WebS/iPhone"))
+        
+        setUI()
     }
 
-
+    @IBAction func tsugi() {
+        
+        
+        if index < mentorArray.count - 1 {
+            
+            index = index + 1
+            
+        }else{
+            
+            index = 0
+            
+        }
+        
+        setUI()
+        
+    }
+    
+    @IBAction func mae() {
+        
+        if index == 0 {
+            
+            index = mentorArray.count - 1
+            
+        }else{
+            
+            index = index - 1
+            
+        }
+        
+        setUI()
+        
+    }
+    
+    
 }
 
